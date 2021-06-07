c0d14b3d7154c1d3199d749b91aaa249579b640d
Sergey Glebov <36161564+SadWaifu@users.noreply.github.com>
2021-06-07 18:38:01 +0300

    init

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        GarbageVector objVector = new GarbageVector(62.9, 45.1, 15.5);
+        System.out.println(objVector);
+        System.out.println(objVector.getLength());
+    }
+}
